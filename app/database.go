4a392d5e2abff01cc3e6835f224f732a9bda4c54
Muhamamad Aris <mhdarisid@gmail.com>
2023-11-20 20:34:22 +0700

    init

---
+++ app/database.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/mhd-aris/task-5-pbi-btpns-muhammad-aris/config"
+	"github.com/mhd-aris/task-5-pbi-btpns-muhammad-aris/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+
+func InitDB() *gorm.DB{
+	dbConfig := config.GetDatabaseConfig()
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require TimeZone=Asia/Jakarta",
+		dbConfig.Host, dbConfig.Port, dbConfig.Username, dbConfig.Password, dbConfig.DBName)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	db.Debug()
+	return db
+}
+
+func MigrateDB(db *gorm.DB) error{
+	db.AutoMigrate(&models.User{})
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mhd-aris/task-5-pbi-btpns-muhammad-aris/app"
+	"github.com/mhd-aris/task-5-pbi-btpns-muhammad-aris/config"
+	"github.com/mhd-aris/task-5-pbi-btpns-muhammad-aris/models"
+)
+
+func init(){
+	config.Load()
+}
+
+
+
+
+func main(){
+	r := gin.Default()
+
+	db := app.InitDB()
+	if err := app.MigrateDB(db); err != nil {
+		panic(err)
+	}
+
+	r.GET("/ping", func(c * gin.Context){
+			
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong!",
+		})
+	})
+	r.POST("/pong", func(c * gin.Context){
+		var user models.User
+
+		user.Username= "Test"
+		user.Email= "test@mail"
+		user.Password= "123"
+		
+		result := db.Create(&user)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error":   result.Error.Error(),
+				"message": "Failed to create user",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong!",
+			"data": user,
+		})
+	})
+
+	r.Run()
+}
